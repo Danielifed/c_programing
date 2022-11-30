e5246cfa52b3537aaf7e022e18dc38e03c567340
Danielifed <d.danielife@gmail.com>
2022-11-30 16:47:09 +0100

    typo correction

--- practice1.c
+++ practice1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//alogrithim that return the sum, product and divison of two set of numbers
+//algorithm that return the sum, product and divison of two set of numbers
 
 int main(void)
 {
